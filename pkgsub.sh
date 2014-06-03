428fdc070c9d88d3c6fc950951fd77c992317475
Narrat <autumn-wind@web.de>
2014-06-03 03:11:36 +0200

    pkgsub: Simplified usage

--- pkgsub.sh
+++ pkgsub.sh
@@ -5,7 +5,10 @@
 # Useful if ABS got no update, or a [testing] PKGBuild is required.
 
 if [ $# -ne 2 ] ; then
-	echo "Usage: $0 <repo> <package>"
+	echo "Usage: $0 <server> <package>"
+    echo "Server:"
+    echo "main (Core, Extra, Testing)"
+    echo "com (Community, Multilib)"
 	exit $E_NO_ARGS
 fi
 
@@ -13,12 +16,14 @@ PAKNAME=$2
 REPO=$1
 
 echo "Checking out server"
-if [ "$REPO" == "core" ] || [ "$REPO" == "extra" ] ; then
+if [ "$REPO" == "main" ] ; then
+    # SVN-Server for [Core], [Extra] and [Testing]
 	svn checkout --depth=empty svn://svn.archlinux.org/packages
 	cd packages
 fi
 
-if [ "$REPO" == "com" ] || [ "$REPO" == "multi" ] ; then
+if [ "$REPO" == "com" ] ; then
+    # SVN-Server for [Community], [Multilib]
 	svn checkout --depth=empty svn://svn.archlinux.org/community
 	cd community
 fi
@@ -31,10 +36,9 @@ echo "...done"
 echo -e "\nMove folder"
 mv -v "$PAKNAME" "$HOME/Builds/ABS/_New/"
 cd ..
-if [ "$REPO" == "core" ] || [ "$REPO" == "extra" ] ; then
+if [ "$REPO" == "main" ] ; then
 	rm -r packages
-fi
-if [ "$REPO" == "com" ] || [ "$REPO" == "multi" ] ; then
+else
 	rm -r community
 fi
 echo "...done"
