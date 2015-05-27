4a44834b7674a8f6fe85255b6ca422eae1eee640
Narrat <autumn-wind@web.de>
2015-05-27 21:00:39 +0200

    print_hash: hacked together for thunar custom actions
    Offers a list of hashsum algos and offers the result via notify-send (dunst in this case).
    Not using (and do not intend to) zenity and likes.
    
    Primary intended for using with Thunar; For the cli this doesn't has a benefit

---
+++ print_hash.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Script for Thunar Custom Actions
+# Click on file, select Hashsum (if that's the TCA name)
+# get a dmenu where you choose the hash algo
+# result via dunst (or zenity, or dzen2)
+
+if [ $# -ne 1 ] ; then
+    echo "Usage: $0 File"
+    exit $E_NO_ARGS
+fi
+
+INPUT=$1
+
+DMENU='dmenu -i -b'
+menitem=$(echo -e "MD5\nSHA1\nSHA224\nSHA256\nSHA384\nSHA512" | $DMENU)
+
+case "$menitem" in
+  MD5)      notify-send MD5:\ $(md5sum ${INPUT} | awk '{print $2}{print $1}') ;;
+  SHA1)     notify-send SHA1:\ $(sha1sum ${INPUT} | awk '{print $2}{print $1}') ;;
+  SHA224)   notify-send SHA224:\ $(sha224sum ${INPUT} | awk '{print $2}{print $1}') ;;
+  SHA256)   notify-send SHA256:\ $(sha256sum ${INPUT} | awk '{print $2}{print $1}') ;;
+  SHA384)   notify-send SHA384:\ $(sha384sum ${INPUT} | awk '{print $2}{print $1}') ;;
+  SHA512)   notify-send SHA512:\ $(sha512sum ${INPUT} | awk '{print $2}{print $1}') ;;
+esac
