32055e493d7d2ad96bbd2353d8ffd1efb8e569cb
Narrat <autumn-wind@web.de>
2015-08-14 02:16:30 +0200

    print_hash: Remove redundant code
    Bothered me from the beginning, but not enough to change it.
    Last commit was enough, so I updated my shell knowledge a bit (functions and case) to get rid of the redundant code

--- print_hash.sh
+++ print_hash.sh
@@ -10,16 +10,17 @@ if [ $# -ne 1 ] ; then
     exit $E_NO_ARGS
 fi
 
+function createmessage {
+    # ${var,} = to upper; ${var,,} = to lower
+    ${1,,}sum "${INPUT}" | awk '{out=$2; for(i=3;i<=NF;i++){out=out" "$i}; print out}{print $1}'
+}
+
 INPUT=$1
 
 DMENU='dmenu -i -b'
 menitem=$(echo -e "MD5\nSHA1\nSHA224\nSHA256\nSHA384\nSHA512" | $DMENU)
 
 case "$menitem" in
-  MD5)      notify-send "MD5: $(md5sum "${INPUT}" | awk '{out=$2; for(i=3;i<=NF;i++){out=out" "$i}; print out}{print $1}')" ;;
-  SHA1)     notify-send "SHA1: $(sha1sum "${INPUT}" | awk '{out=$2; for(i=3;i<=NF;i++){out=out" "$i}; print out}{print $1}')" ;;
-  SHA224)   notify-send "SHA224: $(sha224sum "${INPUT}" | awk '{out=$2; for(i=3;i<=NF;i++){out=out" "$i}; print out}{print $1}')" ;;
-  SHA256)   notify-send "SHA256: $(sha256sum "${INPUT}" | awk '{out=$2; for(i=3;i<=NF;i++){out=out" "$i}; print out}{print $1}')" ;;
-  SHA384)   notify-send "SHA384: $(sha384sum "${INPUT}" | awk '{out=$2; for(i=3;i<=NF;i++){out=out" "$i}; print out}{print $1}')" ;;
-  SHA512)   notify-send "SHA512: $(sha512sum "${INPUT}" | awk '{out=$2; for(i=3;i<=NF;i++){out=out" "$i}; print out}{print $1}')" ;;
+  MD5|SHA*)
+    notify-send "${menitem}: $(createmessage ${menitem})" ;;
 esac
