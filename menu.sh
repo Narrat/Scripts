88f4d0909e18fd2d89e6ad8b547f7ae6e34eea4f
Narrat <autumn-wind@web.de>
2016-02-22 00:31:31 +0100

    Menu: Add weechat

--- menu.sh
+++ menu.sh
@@ -9,16 +9,17 @@
 
 #DMENU='dmenu -i -b'
 DMENU='rofi -hide-scrollbar -columns 15 -dmenu -l 1 -i -p Menu:'
-menitem=$(echo -e "ALSA\nBPython\ncanto\nGnuplot\nJulia\nncmpc\nOctave\nSmuxi-STFL\nViFM" | $DMENU)
+menitem=$(echo -e "ALSA\nBPython\nCanto\nGnuplot\nJulia\nncmpc\nOctave\nSmuxi-STFL\nViFM\nWeeChat" | $DMENU)
 
 case "$menitem" in
   ALSA)         termite --role=Audio -t ALSA -e alsamixer ;;
   BPython)      termite --role=Science -d ${HOME}/Programmieren/Sprachen/Python --title=BPython -e bpython ;;
-  canto)        termite --role=Science -t canto -e /usr/bin/canto-curses ;;
+  Canto)        termite --role=Science -t canto -e /usr/bin/canto-curses ;;
   Gnuplot)      termite --role=Science -d ${HOME}/Gnuplot --title=Gnuplot -e gnuplot ;;
   Julia)        termite --role=Science -d ${HOME}/Octave/JuliaPort --title=Julia -e julia ;;
   ncmpc)        termite --role=Audio -t ncmpc -e ncmpc ;;
   Octave)       termite --role=Science -d ${HOME}/Octave --title=Octave -e octave-cli ;;
   Smuxi-STFL)   termite --role=Smuxi --geometry=110x45 -t Smuxi-STFL -e smuxi-frontend-stfl ;;
   ViFM)         termite --role=FileMan --geometry=110x45 -t ViFM -e vifm ;;
+  WeeChat)      termite --role=WeeChat -e weechat ;;
 esac
