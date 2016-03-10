528174a0cc7317a183691c2596333e005426163f
Narrat <autumn-wind@web.de>
2016-03-10 17:55:17 +0100

    zramasswap: Permission and check for root

--- zramasswap.sh
+++ zramasswap.sh
@@ -2,7 +2,13 @@
 
 # For testing purposes only. There exist better alternatives
 
-sudo modprobe zram
-sudo zramctl -f --size 1024m
-sudo mkswap /dev/zram0
-sudo swapon /dev/zram0 -p 100
+# check if root
+if [[ $EUID -ne 0 ]]; then
+   echo "You must be root to run this script. Aborting...";
+   exit 1;
+fi
+
+modprobe zram
+zramctl -f --size 1024m
+mkswap /dev/zram0
+swapon /dev/zram0 -p 100
