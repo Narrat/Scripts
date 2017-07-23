ff6028d764b84acddd86ab967ee0e69764bb73ae
Narrat <autumn-wind@web.de>
2017-07-23 21:58:29 +0200

    udict: Fix script

--- udict.py
+++ udict.py
@@ -32,18 +32,27 @@ conn = geturl.connectto(url, search)
 
 # Check, get and parse the result
 res, status = geturl.checkresponse(conn, url)
-meaning = re.findall("meaning'>\n.*?<div class='example", res, re.DOTALL)
+#meaning = re.findall("meaning'>\n.*?<div class='example", res, re.DOTALL)
+meaning = re.findall("class=\"word.*?class=\"tags", res, re.DOTALL)
 
 # Remove unnecessary chars
 for k in range(0, len(meaning)):
-    meaning[k] = meaning[k].lstrip("meaning'>\n")
-    meaning[k] = meaning[k].rstrip('</div>\n<div class=\'example')
+    re_dump = re.search("class.*?meaning\">", meaning[k], re.DOTALL)
+    if re_dump is not None:
+        meaning[k] = meaning[k].replace(str(re_dump.group()), '')
+
+    re_dump = "reset"
+    while re_dump is not None:
+        re_dump = re.search("<a href.*?>", meaning[k], re.DOTALL)
+        if re_dump is not None:
+            meaning[k] = meaning[k].replace(str(re_dump.group()), '')
+
     meaning[k] = meaning[k].replace('<br/>', '\n    ')
     meaning[k] = meaning[k].replace('<br', '')
-    meaning[k] = meaning[k].replace("<a href=\"/define.php?term=", '(UD: ')
-    meaning[k] = meaning[k].replace("\">", ' ) ')
+    meaning[k] = meaning[k].replace("</div><div class=\"example\">", '\n    ')
+    meaning[k] = meaning[k].replace("</div><div class=\"tags", '')
     meaning[k] = meaning[k].replace("</a>", '')
-    meaning[k] = unescape(meaning[k].replace("\r", ''))
+    meaning[k] = unescape(meaning[k])
 
 # Set the printing to max 10
 anz = 10 if len(meaning) > 10 else len(meaning)
