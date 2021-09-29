845ea617bfcc4064c3fc478952093d70bc85c079
Narrat <autumn-wind@web.de>
2021-09-29 19:13:47 +0200

    gvfs-archive: simplify call
    Not necessary to add the file://

--- gvfs-archive.sh
+++ gvfs-archive.sh
@@ -2,7 +2,6 @@
 # ToDo:
 # * Cover unmount
 # * Little help
-# * Check if URL-encode construct is right (it works, but looks odd)
 # * rename
 
 # https://andy.wordpress.com/2008/09/17/urlencode-in-bash-with-perl/
@@ -14,4 +13,4 @@ then
     exit 1
 fi
 
-gio mount "archive://$( ( echo -n 'file://' ; readlink -f "$1" ; ) | perl -MURI::Escape -lne 'print uri_escape($_)')"
+gio mount "archive://$( readlink -f "$1" | perl -MURI::Escape -lne 'print uri_escape($_)')"
